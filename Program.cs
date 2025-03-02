008d68b903a132402cc2bacff941da1f88e16ded
kissagnesa <sindzse88@gmail.com>
2025-03-02 11:57:39 +0100

    class & program.cs is done

--- Program.cs
+++ Program.cs
@@ -1,15 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Vonatok
+public class Vonatok
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<Varakozas> varakozasok = BeolvasVarakozasok("varakozas.txt");
+
+        // 1. feladat
+        Console.WriteLine("1. feladat: Várakozások beolvasása");
+        foreach (Varakozas varakozas in varakozasok)
         {
+            Console.WriteLine(varakozas);
         }
+
+        // 2. feladat
+        Console.WriteLine("\n2. feladat: Várakozások száma: " + varakozasok.Count);
+
+        // 3. feladat
+        Console.WriteLine("\n3. feladat: Kérek egy vonalszámot: ");
+        string vonal = Console.ReadLine();
+        List<Varakozas> adottVonalVarakozasok = varakozasok.FindAll(v => v.Vonal == vonal);
+        if (adottVonalVarakozasok.Count > 0)
+        {
+            Console.WriteLine("Várakozások a(z) " + vonal + " vonalon:");
+            foreach (Varakozas varakozas in adottVonalVarakozasok)
+            {
+                Console.WriteLine(varakozas);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nincs várakozás ezen a vonalon.");
+        }
+
+        // 4. feladat
+        Console.WriteLine("\n4. feladat: Esztergom felől érkező vonatokra várakozó állomások:");
+        var esztergomVarakozasok = varakozasok.FindAll(v => v.VarakozikE("Esztergom"));
+        if (esztergomVarakozasok.Count > 0)
+        {
+            foreach (Varakozas varakozas in esztergomVarakozasok)
+            {
+                Console.WriteLine(varakozas.Allomas);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nincs ilyen állomás.");
+        }
+
+        Console.ReadKey();
+    }
+
+    public static List<Varakozas> BeolvasVarakozasok(string fajlnev)
+    {
+        List<Varakozas> varakozasok = new List<Varakozas>();
+        try
+        {
+            string[] sorok = File.ReadAllLines(fajlnev);
+            for (int i = 1; i < sorok.Length; i++) // Az első sor a fejléc
+            {
+                varakozasok.Add(new Varakozas(sorok[i]));
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("A 'varakozas.txt' fájl nem található.");
+        }
+        return varakozasok;
     }
 }
---
+++ Varakozas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Varakozas
+{
+    public string Vonal { get; set; }
+    public string Allomas { get; set; }
+    public string ErkezoIrany { get; set; }
+    public string InduloIrany { get; set; }
+    public int MaxKeses { get; set; }
+
+    public Varakozas(string vonal, string allomas, string erkezoIrany, string induloIrany, int maxKeses)
+    {
+        Vonal = vonal;
+        Allomas = allomas;
+        ErkezoIrany = erkezoIrany;
+        InduloIrany = induloIrany;
+        MaxKeses = maxKeses;
+    }
+
+    public Varakozas(string sor)
+    {
+        string[] adatok = sor.Split('\t');
+        Vonal = adatok[0];
+        Allomas = adatok[1];
+        ErkezoIrany = adatok[2];
+        InduloIrany = adatok[3];
+        MaxKeses = int.Parse(adatok[4]);
+    }
+
+    public override string ToString()
+    {
+        return $"{Vonal} - {Allomas} - Érkező: {ErkezoIrany} - Induló: {InduloIrany} - Max késés: {MaxKeses} perc";
+    }
+
+    public bool VarakozikE(string erkezo)
+    {
+        if (ErkezoIrany == "Összes")
+        {
+            return true;
+        }
+        string[] erkezoAllomasok = ErkezoIrany.Split(',');
+        return erkezoAllomasok.Contains(erkezo);
+    }
+}
